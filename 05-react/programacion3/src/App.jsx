b2c335c255052edde698135965a791dc84f16f63
Ale Luna <lunalebizkit@gmail.com>
2021-06-04 21:14:21 -0300

    cree react

---
+++ 05-react/programacion3/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+function App () {
+    return (
+        <>
+        <h1>Primera Aplicacion de React</h1>
+        <button>+</button>
+        <button>-</button>
+        </>
+    );
+
+};
+export default App;
