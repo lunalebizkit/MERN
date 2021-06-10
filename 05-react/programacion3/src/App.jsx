3e2fe131cfe225fdbbe9db333930ae43b697f984
lunalebizkit <lunalebizkit@gmail.com>
2021-06-10 14:11:12 -0300

    intentando component parents y child

--- 05-react/programacion3/src/App.jsx
+++ 05-react/programacion3/src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
-import Contador from './components/Contador'
+import Contador from './components/Contador';
+import Menu from './components/Menu';
+import Parent from './components/Parent';
 
 
 
 function App () {
     return (
         <>
+        <Parent />
         <Contador />
+        <Menu />
         </>
     );
 
