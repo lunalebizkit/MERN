fd1776d5260ec64c74b51fc3c9643778c0448a75
lunalebizkit <lunalebizkit@gmail.com>
2021-10-06 18:43:13 -0300

    funciona

--- 05-react/programacion3/frontEnd/src/acciones/equipos.js
+++ 05-react/programacion3/frontEnd/src/acciones/equipos.js
@@ -1,6 +1,4 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-const BASE_URL='http://localhost:4000';
+const BASE_URL='http://localhost:4000/api/tareas';
 export const GetEquipo=createAsyncThunk('equipo/getEquipo', async ()=>{await axios.get(`${BASE_URL}/equipos`)}); 
-const hola=async  ()=> console.log('esto es algo' + await axios.get(`${BASE_URL}/equipos`))
-hola()
--- 05-react/programacion3/frontEnd/src/paginas/paginaAgregarEquipo.jsx
+++ 05-react/programacion3/frontEnd/src/paginas/paginaAgregarEquipo.jsx
@@ -1,23 +1,47 @@
 import React from "react";
-const paginaAgregarEquipo= ()=>{
-    const agregarEquipo=()=> console.log("agrega")
+import axios from "axios";
+import { useState } from "react";
+const PaginaAgregarEquipo= ()=>{
+    const [valor, setValor]= useState({nombre: '', deporte: ''});
+    const handleChange= (e) => {
+        const {name, value}= e.target;
+        setValor({...valor,  [name]: value})
+       
+    }
+    const handleSubmit= (e)=>{
+        axios.post('http://localhost:4000/api/tareas/equipos', valor)
+        .then(res => console.log(res.data))
+        .catch(res => console.log(res))
+        e.preventDefault();
+        console.log(valor)}
+    // constructor() {
+    //     super();
+    //     this.state= {
+    //         nombre: '',
+    //         deporte: ''
+    //     }
+    //     this.agregarEquipo= this.agregarEquipo.bind(this)
+    // }
+    // const agregarEquipo= e=> setEstado(e.target.value);
+      
+       const {nombre, deporte}= valor
     return(
         <>
         <div className="container">
             <div className="row">
-                <div className="col s5">
+                <div className="col s5">                    
                     <div className="card">
                         <div className="card-content">
-                            <form onSubmit={agregarEquipo} action="">
+                            <form onSubmit={handleSubmit} action="">
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input type="text" placeholder="Agregar Equipo" />
+                                        <input name="nombre" type="text" value={nombre} onChange={handleChange} placeholder="Agregar Equipo" />
 
                                     </div>
                                 </div>
                                 <div className="row">
                                     <div className="input-field col s12">
-                                        <input type="text" placeholder="Agregar Deporte" />
+                                        <input name="deporte" type="text" value={deporte} onChange={handleChange} placeholder="Agregar Deporte" />
 
                                     </div>
                                 </div>
@@ -26,10 +50,13 @@ const paginaAgregarEquipo= ()=>{
                         </div>
                     </div>
                 </div>
+                <div className="col s7">
+
+                </div>
             </div>
         </div>
         </>
     )
 
 }
-export default paginaAgregarEquipo
+export default PaginaAgregarEquipo
