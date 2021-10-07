c6c2f7f50ded0356f65b421ea3b9ccae6d948155
lunalebizkit <lunalebizkit@gmail.com>
2021-10-07 08:33:55 -0300

    obtener equipos

---
+++ 05-react/programacion3/frontEnd/src/paginas/ObtenerEquipos.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import axios from "axios";
+export default function ObtenerEquipo(){
+    // const traerEquipo= ()=>{
+    //     axios('http://localhost:4000/api/tareas/equipos')
+    //     .then(res => res.json())
+    //     .then(data => console.log(data))
+    // }
+    // return(
+    //     <>
+    //     {traerEquipo}
+    //     </>
+    // )
+}
