57208e34f75fa953fd6457f3d5bd762264251507
lunalebizkit <lunalebizkit@gmail.com>
2021-10-17 16:28:53 -0300

    agregue dashboard

---
+++ 05-react/programacion3/frontEnd/src/MiRuteador.jsx
@@ -0,0 +1,20 @@
+import { Switch, Route, Link } from "react-router-dom";
+import Contador from './components/Contador';
+import PaginaEquipo from './paginas/paginaEquipo';
+import PaginaAgregarEquipo from './paginas/paginaAgregarEquipo';
+import BarraNavegacion from './components/BarraNavegacion';
+import React from 'react';
+import AgregarTarea from './paginas/AgregarTarea';
+import PaginaInicio from './paginas/Inicio';
+const MiRuteador = ()=>{
+    return(
+        <Switch>
+              <Route exact path="/" component={PaginaInicio} />
+              <Route exact path="/contador" component={Contador} />
+              <Route exact path="/paginaEquipo" component={PaginaEquipo} />
+              <Route exact path="/paginaAgregarEquipo" component={PaginaAgregarEquipo} />
+              <Route exact path="/paginaAgregarTarea" component={AgregarTarea} />
+        </Switch>
+    )
+}
+export default MiRuteador;
