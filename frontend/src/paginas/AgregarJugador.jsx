86f77d280d766fc9b58884e835122337b9db047b
lunalebizkit <lunalebizkit@gmail.com>
2021-11-10 18:51:51 -0300

    paper

--- frontend/src/paginas/AgregarJugador.jsx
+++ frontend/src/paginas/AgregarJugador.jsx
@@ -1,9 +1,19 @@
 import * as React from 'react';
+import Grid from '@mui/material/Grid';
+import { createStyles } from '@mui/material/styles'
+import { yellow } from '@mui/material/colors';
+import Paper from '@mui/material/Paper';
+import Box from '@mui/material/Box'
 import { useParams } from 'react-router';
 export default function AgregarJugador() {
     let {id}= useParams()
-    console.log(id)
     return(<>
-    <h1>{id}Holaaa</h1>
+    <Grid>
+        <Paper sx={{bgcolor: '#cfe8fc', height: 60, width: 400}} elevation= {6} >
+            <Box><h1>{id}Holaaa</h1></Box>
+        
+        </Paper>
+    </Grid>
+    
     </>)
 }
