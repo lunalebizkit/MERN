803be73905c3e29e2fe2795cdd2339c2f15a284e
lunalebizkit <lunalebizkit@gmail.com>
2021-11-15 14:25:35 -0300

    acutalize filas de jugador

--- frontend/src/components/FilasTablaEquipos.jsx
+++ frontend/src/components/FilasTablaEquipos.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { useDispatch, useSelector } from "react-redux";
-import {Link as RouterLink} from 'react-router-dom';
+import { Link as RouterLink } from 'react-router-dom';
 import { editEquipo } from "../store/slice/editEquipoSlice";
 import { delEquipo } from "../store/slice/delEquipoSlice";
 import TableCell from '@mui/material/TableCell';
@@ -9,34 +9,42 @@ import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit';
 import GroupAddIcon from '@mui/icons-material/GroupAdd';
 import IconButton from '@mui/material/IconButton';
-const FilasTablaEquipos= ()=>{
-    const dispatch= useDispatch()
-    const equipos= useSelector((state)=> state.equipo.listaEquipo)
-    const eliminar= (id)=>{
-        if (window.confirm("¿Estas seguro de eliminar?")) {
+
+const FilasTablaEquipos = () => {
+    const dispatch = useDispatch()
+    const equipos = useSelector((state) => state.equipo.listaEquipo)
+    const eliminar = (id) => {       
+        if(window.confirm("estas Seguro de eliminar?")) {
             dispatch(delEquipo(id))
-        }
-    }
-    const renderizarTablaEquipos=()=>{
-        return equipos.map(({nombre, deporte, _id, jugadores}) =>{ 
-        return(           
+        }}
+    const renderizarTablaEquipos = () => {
+        return equipos.map(({ nombre, deporte, _id, jugadores }) => {
+            return (
                 <TableRow key={_id}>
-                    <TableCell>{nombre}</TableCell>                   
+                    <TableCell>{nombre}</TableCell>
                     <TableCell>{deporte}</TableCell>
                     <TableCell> {jugadores.length}</TableCell>
-                    <TableCell><IconButton color= "secondary"
-                         onClick={()=> dispatch(editEquipo(_id))} >
-                            <EditIcon /></IconButton>
-                        <IconButton onClick={()=>eliminar(_id)} color="error" ><DeleteIcon /></IconButton>
+                    <TableCell><IconButton color="secondary"
+                        onClick={() => dispatch(editEquipo(_id))} >
+                        <EditIcon /></IconButton>
+                        <IconButton onClick={()=>{eliminar(_id)}} color="error" ><DeleteIcon />
+                        </IconButton>
+
                         <IconButton color="primary" component={RouterLink} to={'AgregarJugador/' + _id} >
-                            < GroupAddIcon/>
+
+                            < GroupAddIcon />
                         </IconButton>
                     </TableCell>
-                </TableRow>       
-    )})}
-    return(
+                </TableRow>
+
+
+            )
+        })
+    }
+    return (
         <>
-         {renderizarTablaEquipos()}
+            {renderizarTablaEquipos()}
+          
         </>
     )
 }
